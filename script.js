29a26e5e45173fe41e7a44a85a7b68e7a9b2fab6
Igor-Matheus <igo.mat@hotmail.com>
2024-06-14 00:45:30 -0300

    first version of the game

--- script.js
+++ script.js
@@ -6,12 +6,19 @@ const reset = document.querySelector(".reset")
 let humanScoreNumber = 0
 let machineScoreNumber = 0
 
+// ENUMS
+const GAME_OPTIONS = {
+    ROCK: 'rock',
+    PAPER: 'paper',
+    SCISSORS: 'scissors'
+}
+
 const playHuman = (humanChoice) => {
     playTheGame(humanChoice, playMachine())
 }
 
 const playMachine = () => {
-    const choices = ['rock', 'paper', 'scissors']
+    const choices = [GAME_OPTIONS.ROCK, GAME_OPTIONS.PAPER, GAME_OPTIONS.SCISSORS]
     const randomNumber = Math.floor(Math.random() * 3)
 
     return choices[randomNumber]
@@ -22,7 +29,7 @@ const playTheGame = (human, machine) => {
 
     if (human === machine) {
         result.innerHTML = "Deu empate!"
-    } else if ((human === 'paper' && machine === 'rock') || (human === 'rock' && machine === 'scissors') || (human === 'scissors' && machine === 'paper')) {
+    } else if ((human === GAME_OPTIONS.PAPER && machine === GAME_OPTIONS.ROCK) || (human === GAME_OPTIONS.ROCK && machine === GAME_OPTIONS.SCISSORS) || (human === GAME_OPTIONS.SCISSORS && machine === GAME_OPTIONS.PAPER)) {
         humanScoreNumber++
         yourScore.innerHTML = humanScoreNumber
         result.innerHTML = "Você ganhou!"
